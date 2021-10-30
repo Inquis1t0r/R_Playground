bdbe0ee68d6de519cec8e28ba95206f2b1f742e6
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-10-30 19:38:03 +0200

    Update Zadanie0.r

--- Zadanie0.r
+++ Zadanie0.r
@@ -9,15 +9,18 @@
 #plot(x, sin(x), main = "sinus")
 #plot(x, sin(x), main = "sinus", type="h")
 
-boxplot(Temp~Month, #wykres pudełkowy
-        data=airquality,
-        main="Different boxplots for each month",
-        xlab="Month Number",
-        ylab="Degree Fahrenheit",
-        col="orange",
-        border="black" #wpływa też na kolor linii mediany + wąsy
-)
+#boxplot(Temp~Month, #wykres pudełkowy
+#        data=airquality,
+#        main="Different boxplots for each month",
+#        xlab="Month Number",
+#        ylab="Degree Fahrenheit",
+#        col="orange",
+#        border="black" #wpływa też na kolor linii mediany + wąsy
+#)
+#"" -> komentarz na kilka linii, ale średnio działa ze stringami w parametrach
 
-#mandaty <- c(235, 138, 42, 28, 16, 1)
-#partie <- c("PIS", "PO", "Kukiz'15", "Nowoczesna", "PSL", "mniejszość niemiecka")
-#pie(mandaty, labels = partie, col = rainbow(length(partie)), main = "Sejm RP listopad 2015") #Main: nagłowek
+ceny <- c(5.93, 5.97, 5.96, 5.94, 5.97, 5.95)
+woj <- c("dolnośląskie", "kujawsko-pomorskie", "lubelskie", "lubuskie", "łódzkie", "małopolskie")
+pie(ceny, labels = ceny, col = rainbow(length(woj)), main = "Średnie ceny paliw w Polsce") #Main: nagłowek
+legend("topright", woj, cex = 0.8,
+       fill = rainbow(length(x)))
